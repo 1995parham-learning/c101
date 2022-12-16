1399cc9d9d64483f684725d4301abc87d16dda69
Parham Alvani <parham.alvani@gmail.com>
2022-12-16 12:16:13 +0330

    feat: update cmp-without-if example

--- cmp-without-if/cmp-without-if.c
+++ cmp-without-if/cmp-without-if.c
@@ -1,18 +1,12 @@
-/*
- * In The Name Of God
- * ========================================
- * [] File Name : cmp-without-if.c
- *
- * [] Creation Date : 08-02-2015
- *
- * [] Last Modified : Sun 08 Feb 2015 12:48:14 AM IRST
- *
- * [] Created By : Parham Alvani (parham.alvani@gmail.com)
- * =======================================
- */
 #include <math.h>
 #include <stdio.h>
 
+// max find the maximum between a and b, because:
+// a + b + | a - b |
+// if a > b then
+// a + b + a - b = 2a
+// else
+// a + b + b - a = 2b
 double max(double a, double b) { return 1 / 2. * (a + b + fabs(a - b)); }
 
 int main(int argc, char *argv[]) {
@@ -21,8 +15,9 @@ int main(int argc, char *argv[]) {
 
   printf("%g\n", max(a, b));
 
+  // check whether b is the maximum or not
   int c = ceil(max(a, b) / b);
   c = (c + 1) % c;
-  /* c should be 1 if a > b or else it should be 0 */
+  // c should be 1 if a > b or else it should be 0
   printf("%d\n", c);
 }
